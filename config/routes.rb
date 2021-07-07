61597ee50a38a175fe1b1f53bfee1e9a6e988ba6
karen.orduna <korduna@ttisuccessinsights.fr>
2021-07-07 15:59:50 +0200

    changed title and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'application#homepage'
-  get :discmodel, to: 'application#discmodel'
-  get :workdisc, to: 'application#workdisc'
+  # get :discmodel, to: 'application#discmodel'
+  # get :workdisc, to: 'application#workdisc'
 end
